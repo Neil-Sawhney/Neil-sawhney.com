a84d3bdc8c87f71fb9b0687ee4b0cdb0638fd87c
Neil Sawhney <Neil@neil-sawhney.com>
2022-07-26 03:32:23 -0400

    ik im crazy

--- api/models/dials.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const DialsSchema = mongoose.Schema({
-	_id: mongoose.Schema.Types.ObjectId,
-	data: [Number],
-	status: Boolean,
-});
-
-module.exports = mongoose.model('dials',DialsSchema)
--- api/models/trafficLight.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const trafficLightSchema = mongoose.Schema({
-	_id: mongoose.Schema.Types.ObjectId,
-	color: String
-});
-
-module.exports = mongoose.model('trafficLight',trafficLightSchema)
