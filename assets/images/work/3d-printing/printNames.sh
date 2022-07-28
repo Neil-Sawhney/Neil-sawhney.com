5378d76335db719fdb8514b2f27afdcde3ea9bfd
Neil Sawhney <Neil@neil-sawhney.com>
2022-07-28 03:49:58 -0400

    moved shortcodes to extras

---
+++ assets/images/work/3d-printing/printNames.sh
@@ -0,0 +1,3 @@
+for f in *; do
+	echo "<div class=\"item\" data-image=\"assets/images/work/3d-printing/$f\"></div>";
+done;
---
+++ assets/images/work/photography/printNames.sh
@@ -0,0 +1,3 @@
+for f in *; do
+	echo "<div class=\"item\" data-image=\"assets/images/work/photography/$f\"></div>";
+done;
