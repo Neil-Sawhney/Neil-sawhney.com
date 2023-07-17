0e013a0fb76474f240a93f7e6c3d6d518074173b
Neil Sawhney <Neil@neil-sawhney.com>
2023-07-16 22:01:12 -0400

    code now works without vite

--- articles/dynamics-simulations/main.js
+++ articles/dynamics-simulations/main.js
@@ -1,8 +1,6 @@
-import * as THREE from 'three';
+import * as THREE from './node_modules/three/build/three.module.js';
 
-import { FontLoader } from 'three/addons/loaders/FontLoader.js';
-import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
-import { Clock } from 'three/src/core/Clock.js';
+import { Clock } from './node_modules/three/src/core/Clock.js';
 
 
 const scene = new THREE.Scene();
